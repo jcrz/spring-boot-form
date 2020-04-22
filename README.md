c59af369ed992ce04ac113ecd9d10ba2c0fd9b3f
jcrz <jfuentes.dev@gmail.com>
2020-04-21 23:37:58 -0400

    Métodos handler

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # Apuntes del proyecto
 
+
+## Clase model del formulario
+
+Añadir link al archivo Usuario.java
+
+## Método handler para recibir y procesar los datos enviados
+
+```java
+	// Handler para mostrar el formulario
+	@GetMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("titulo", "Formulario usuarios");
+		return "form";
+	}
+	
+	// Handler para recibir los datos utilizando @RequestParam
+	@PostMapping("/form")
+	public String procesar(Model model, 
+			@RequestParam String username,
+			@RequestParam String password,
+			@RequestParam String email) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setUsername(username);
+		usuario.setPassword(password);
+		usuario.setEmail(email);
+		
+		model.addAttribute("titulo", "Resultado form");
+		model.addAttribute("usuario", usuario);
+		
+		return "resultado";
+	}
+```
+
+## Método handler automatizado
+
+```java
+	// Handler automatizado para recibir los datos
+	@PostMapping("/form")
+	public String procesar(Usuario usuario, Model model) {
+		model.addAttribute("titulo", "Resultado form");
+		model.addAttribute("usuario", usuario);
+		return "resultado";
+	}
+```
--- src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controller/FormController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
 public class FormController {
@@ -11,12 +14,35 @@ public class FormController {
 	// Handler para mostrar el formulario
 	@GetMapping("/form")
 	public String form(Model model) {
+		model.addAttribute("titulo", "Formulario usuarios");
 		return "form";
 	}
 	
-	// Handler para recibir los datos
+	// Handler para recibir los datos utilizando @RequestParam
+	/*
 	@PostMapping("/form")
-	public String procesar(Model model) {
+	public String procesar(Model model, 
+			@RequestParam String username,
+			@RequestParam String password,
+			@RequestParam String email) {
+		
+		Usuario usuario = new Usuario();
+		usuario.setUsername(username);
+		usuario.setPassword(password);
+		usuario.setEmail(email);
+		
+		model.addAttribute("titulo", "Resultado form");
+		model.addAttribute("usuario", usuario);
+		
+		return "resultado";
+	}
+	*/
+	
+	// Handler automatizado para recibir los datos
+	@PostMapping("/form")
+	public String procesar(Usuario usuario, Model model) {
+		model.addAttribute("titulo", "Resultado form");
+		model.addAttribute("usuario", usuario);
 		return "resultado";
 	}
 }
